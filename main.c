374d3b3075db262d70738fc24bec0484edba313e
TimLindholm98 <timpanmili98@gmail.com>
2020-10-15 16:46:40 +0200

    First code test.
    also added custom libary for 
    millis(https://github.com/monoclecat/avr-millis-function)

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
-#include <avr/io.h>
+#define F_CPU 16000000UL
+#define LED (1 << PB0)
 
+#include <avr/io.h>
+#include <stdbool.h> // while(true)
+#include <util/delay.h> // For delay
 #include "serial.h"
 
-void main (void) {
+void wait(uint32_t mSec){
+  _delay_ms(mSec);
+}
 
+void LEDloop(uint32_t mSec){
+  while(true){
+    PORTB = LED;
+    wait(mSec);
+    PORTB = ~LED;
+    wait(mSec);
+  }
+}
+void LEDloop2(uint32_t mSec){
+  while(true){
+    PORTB = (1 << 0);
+    wait(mSec);
+    PORTB = ~(1 << 0);
+    wait(mSec);
+  }
+}
+
+void main (void) {
+  DDRB &= ~(1<<PB0); //PORTBIT0 == OUTPUT
+
+  LEDloop(500);
+  LEDloop2(500);
 }
