8c8c1374e14fc6f03f30565727ee8c1cd6a378e4
TimLindholm98 <timpanmili98@gmail.com>
2020-10-18 23:44:58 +0200

    updates

--- avrmillis.c
+++ avrmillis.c
@@ -3,7 +3,6 @@
 #include <avr/interrupt.h>
 
 
-
 volatile unsigned long timer1_millis;
 //NOTE: A unsigned long holds values from 0 to 4,294,967,295 (2^32 - 1). It will roll over to 0 after reaching its maximum value.
 
--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#define F_OSC 16000000UL
+
 #include <avr/io.h>
 #include <stdbool.h> // while(true)
 #include <util/delay.h> // For delay
@@ -7,8 +9,12 @@
 
 void main(void){
   uart_init();
+  /*init_millis(F_OSC);
+  sei();*/
+  static unsigned long current_millis = 0;
 
   while(1){
+
     char myName[] = "TimLindholm\n";
     uart_putstr(myName);
     wait(1000);
